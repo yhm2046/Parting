785c04962829c38afd1858d6ceea877fe7be0421
yhm2046 <yhm2046@gmail.com>
2021-04-14 14:16:14 +0800

    添加点击后改变BackgroundResource

--- app/src/main/java/com/viomi/parting/PartingActivity2.kt
+++ app/src/main/java/com/viomi/parting/PartingActivity2.kt
@@ -25,9 +25,25 @@ class PartingActivity2 : AppCompatActivity() {
         gridView = blind.gridView
         val mainAdapter = MyAdapter(this, playerNames, playerImages)
         blind.gridView.adapter = mainAdapter
-        blind.gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            Toast.makeText(applicationContext, "You CLicked " + playerNames[+position],
-                    Toast.LENGTH_SHORT).show()
+        /**
+         * Callback method to be invoked when an item in this AdapterView has
+         * been clicked.
+         * <p>
+         * Implementers can call getItemAtPosition(position) if they need
+         * to access the data associated with the selected item.
+         *
+         * @param parent The AdapterView where the click happened.
+         * @param view The view within the AdapterView that was clicked (this
+         *            will be a view provided by the adapter)
+         * @param position The position of the view in the adapter.
+         * @param id The row id of the item that was clicked.
+         */
+        blind.gridView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+//            Toast.makeText(applicationContext, "You CLicked " + playerNames[+position],
+//                    Toast.LENGTH_SHORT).show()
+//            view.setBackgroundColor(Color.RED)
+            view.setBackgroundResource(R.drawable.ic_launcher_foreground)
+            Log.d(tag,"parent:$parent,view:$view,position:$position,id:$id")
             Log.d(tag,"You CLicked ${playerNames[position]}")
         }
     }//end oncreate
